0d14aa605cf1a55b4e46ca845b0160d3f69bbdfb
Ahmed Mohammed <eng.a7medfci@gmail.com>
2021-10-01 05:55:05 +0200

    first commit

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -29,7 +29,7 @@ class GoogleController extends Controller
     {
         try {
 
-            $user = Socialite::driver('google')->user();
+            $user = Socialite::driver('google')->stateless()->user();
             $finduser = User::where('google_id', $user->id)->first();
             if($finduser){
 
